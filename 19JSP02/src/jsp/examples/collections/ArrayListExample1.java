2ca6a5ae9693450d3f4b5c88ee151772b5b10aba
tranthanhcry123456 <139839807+tranthanhcry123456@users.noreply.github.com>
2024-12-31 10:23:44 +0700

    Update ArrayListExample1.java
    gf123

--- 19JSP02/src/jsp/examples/collections/ArrayListExample1.java
+++ 19JSP02/src/jsp/examples/collections/ArrayListExample1.java
@@ -15,7 +15,7 @@ public class ArrayListExample1 {
         // Creating arraylist
         ArrayList<String> list = new ArrayList<String>();
         // Add objects to arraylist
-        list.add("Java");
+        list.add("1243Java");
         list.add("C++");
         list.add("PHP");
         list.add("Java");
