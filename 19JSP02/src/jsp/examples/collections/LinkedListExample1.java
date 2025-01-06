1bba021ac350878159147d17018dee7224bd5943
tranthanhcry123456 <139839807+tranthanhcry123456@users.noreply.github.com>
2025-01-06 23:29:55 +0700

    Update LinkedListExample1.java
    fsd21

--- 19JSP02/src/jsp/examples/collections/LinkedListExample1.java
+++ 19JSP02/src/jsp/examples/collections/LinkedListExample1.java
@@ -21,7 +21,7 @@ public class LinkedListExample1 {
         linkedList.add("Java");
         // Show list through Iterator
         Iterator<String> itr = linkedList.iterator();
-        while (itr.hasNex234t()) {
+        while (itr.has123Nex234t()) {
             System.out.print(itr.next() + ", ");
         }
         // add first
